500a9af6a740881229c507c007fd28882465f130
oykos-development-hub <hub@oykos.me>
2023-07-10 15:21:59 +0000

    Update code from GitLab

--- shared/get_data_root.go
+++ shared/get_data_root.go
@@ -9,18 +9,21 @@ import (
 
 func GetDataRoot() string {
 	var root = "http://localhost:8080/mocked-data"
+	var environment string
 
 	if len(os.Args) > 1 {
-		environment := os.Args[1]
+		environment = os.Args[1]
+	} else if os.Getenv("ENVIRONMENT") != "" {
+		environment = os.Getenv("ENVIRONMENT")
+	}
 
-		if environment == "staging" {
-			fmt.Println("Staging environment!")
-		} else if environment == "development" {
-			// Disable certificate verification
-			http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
-			fmt.Println("Development environment!")
-			root = "https://sss-erp-bff.oykos.me/mocked-data"
-		}
+	if environment == "staging" {
+		fmt.Println("Staging environment!")
+	} else if environment == "development" {
+		// Disable certificate verification
+		http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+		fmt.Println("Development environment!")
+		root = "https://sss-erp-bff.oykos.me/mocked-data"
 	}
 
 	return root

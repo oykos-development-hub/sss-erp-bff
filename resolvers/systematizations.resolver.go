4f843c92d446c3b0376aa5aa7af1b9748d490952
oykos-development-hub <hub@oykos.me>
2023-08-11 09:51:54 +0000

    Update code from GitLab

--- resolvers/systematizations.resolver.go
+++ resolvers/systematizations.resolver.go
@@ -6,7 +6,6 @@ import (
 	"bff/shared"
 	"bff/structs"
 	"encoding/json"
-	"fmt"
 	"strconv"
 
 	"github.com/graphql-go/graphql"
@@ -123,7 +122,6 @@ var SystematizationInsertResolver = func(params graphql.ResolveParams) (interfac
 }
 
 var SystematizationDeleteResolver = func(params graphql.ResolveParams) (interface{}, error) {
-	fmt.Println("Brisanje sistematizacije")
 	itemId := params.Args["id"]
 
 	if !shared.IsInteger(itemId) && !(itemId.(int) <= 0) {

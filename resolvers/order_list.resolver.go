abe6dc92b1480481f0c52ba09e1767fce6bb21d2
oykos-development-hub <hub@oykos.me>
2023-11-01 09:04:52 +0000

    Update code from GitLab

--- resolvers/order_list.resolver.go
+++ resolvers/order_list.resolver.go
@@ -283,7 +283,7 @@ var OrderProcurementAvailableResolver = func(params graphql.ResolveParams) (inte
 
 	for _, item := range articles {
 		currentArticle := item // work with a copy to avoid modifying the range variable
-
+		currentArticle.Price = currentArticle.NetPrice
 		getOrderProcurementArticleInput := dto.GetOrderProcurementArticleInput{
 			ArticleID: &currentArticle.Id,
 		}
@@ -299,7 +299,6 @@ var OrderProcurementAvailableResolver = func(params graphql.ResolveParams) (inte
 				currentArticle.TotalPrice = currentArticle.TotalPrice * float32(currentArticle.Available-orderArticle.Amount/currentArticle.Available)
 				currentArticle.TotalPrice = currentArticle.TotalPrice * float32(currentArticle.Available-orderArticle.Amount/currentArticle.Available)
 				currentArticle.Available -= orderArticle.Amount
-				currentArticle.Price = currentArticle.NetPrice
 			}
 		}
 		items = append(items, currentArticle)

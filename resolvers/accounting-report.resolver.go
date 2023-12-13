cd7fb00313d5179eb7ba334ace0f0523f1fead97
oykos-development-hub <hub@oykos.me>
2023-12-13 13:53:45 +0000

    Update code from GitLab

--- resolvers/accounting-report.resolver.go
+++ resolvers/accounting-report.resolver.go
@@ -5,7 +5,6 @@ import (
 	"bff/dto"
 	"bff/shared"
 	"encoding/json"
-	"strconv"
 	"strings"
 
 	"github.com/graphql-go/graphql"
@@ -31,60 +30,15 @@ var OverallSpendingResolver = func(params graphql.ResolveParams) (interface{}, e
 
 	var response []dto.ArticleReport
 
-	if data.OrganizationUnitID != nil && *data.OrganizationUnitID > 0 {
-		organizationUnitID := strconv.Itoa(*data.OrganizationUnitID)
-		res, err := getOfficeDropdownSettings(&dto.GetOfficesOfOrganizationInput{
-			Value: &organizationUnitID,
-		})
-
-		if err != nil {
-			return shared.HandleAPIError(err)
-		}
-
-		for _, article := range articles {
-			for _, office := range res.Data {
-				if article.OfficeID == office.Id {
-					found := false
-					for i, existingArticle := range response {
-						if article.Title == existingArticle.Title && article.Year == existingArticle.Year && article.Description == existingArticle.Description {
-							response[i].Amount += article.Amount
-							found = true
-							break
-						}
-					}
-
-					if !found {
-						if !flagArticles {
-							response = append(response, article)
-						} else {
-							for _, articleString := range data.Articles {
-								parts := strings.SplitN(articleString, " ", 2)
-								if len(parts) != 2 {
-									continue
-								}
-								if strings.Contains(parts[0], article.Year) && strings.Contains(parts[1], article.Title) {
-									response = append(response, article)
-								}
-
-							}
-						}
-					}
-				}
-			}
-		}
+	if !flagArticles {
+		response = articles
 	} else {
-		if !flagArticles {
-			response = articles
-		} else {
-			for _, article := range articles {
-				for _, articleString := range data.Articles {
-					parts := strings.SplitN(articleString, " ", 2)
-					if len(parts) != 2 {
-						continue
-					}
-					if strings.Contains(parts[0], article.Year) && strings.Contains(parts[1], article.Title) {
-						response = append(response, article)
-					}
+		for _, article := range articles {
+			for _, articleString := range data.Articles {
+				parts := strings.SplitN(articleString, " ", 2)
+
+				if strings.Contains(parts[0], article.Year) && strings.Contains(parts[1], article.Title) {
+					response = append(response, article)
 				}
 			}
 		}

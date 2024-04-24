d7194c44aa06d6f3b93478e0d25cad0712327f09
oykos-development-hub <hub@oykos.me>
2024-04-24 13:18:29 +0000

    Update code from GitLab

--- internal/api/dto/accounting.go
+++ internal/api/dto/accounting.go
@@ -3,15 +3,13 @@ package dto
 import "time"
 
 type ObligationForAccounting struct {
-	InvoiceID                 *int      `json:"invoice_id"`
-	AdditionalExpenseID       *int      `json:"additional_expense_id"`
-	SalaryAdditionalExpenseID *int      `json:"salary_additional_expense_id"`
-	Type                      string    `json:"type"`
-	Title                     string    `json:"title"`
-	TotalPrice                float64   `json:"total_price"`
-	RemainPrice               float64   `json:"remain_price"`
-	Status                    string    `json:"status"`
-	CreatedAt                 time.Time `json:"created_at"`
+	InvoiceID *int      `json:"invoice_id"`
+	SalaryID  *int      `json:"salary_id"`
+	Type      string    `json:"type"`
+	Title     string    `json:"title"`
+	Price     float64   `json:"price"`
+	Status    string    `json:"status"`
+	CreatedAt time.Time `json:"created_at"`
 }
 
 type GetObligationsForAccountingResponseMS struct {

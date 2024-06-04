49d57fea8fadb981104a47f03d578baac215aba7
oykos-development-hub <hub@oykos.me>
2024-06-04 10:03:44 +0000

    Update code from GitLab

--- internal/api/graphql/fields/interal_reallocations.field.go
+++ internal/api/graphql/fields/interal_reallocations.field.go
@@ -27,7 +27,7 @@ func (f *Field) InternalReallocationOverviewField() *graphql.Field {
 			"year": &graphql.ArgumentConfig{
 				Type: graphql.Int,
 			},
-			"requested_by": &graphql.ArgumentConfig{
+			"request_by": &graphql.ArgumentConfig{
 				Type: graphql.Int,
 			},
 			"budget_id": &graphql.ArgumentConfig{

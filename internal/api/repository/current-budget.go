db2431b990bf6f21474160c1648feaf4a2a70ce5
oykos-development-hub <hub@oykos.me>
2024-06-15 11:38:18 +0000

    Update code from GitLab

--- internal/api/repository/current-budget.go
+++ internal/api/repository/current-budget.go
@@ -1,12 +1,19 @@
 package repository
 
 import (
+	"bff/config"
 	"bff/internal/api/dto"
 	"bff/structs"
 	"context"
+	"strconv"
 )
 
 func (repo *MicroserviceRepository) CreateCurrentBudget(ctx context.Context, currentBudget *structs.CurrentBudget) (*structs.CurrentBudget, error) {
+	header := make(map[string]string)
+
+	account := ctx.Value(config.LoggedInAccountKey).(*structs.UserAccounts)
+	header["UserID"] = strconv.Itoa(account.ID)
+
 	res := dto.GetCurrentBudgetResponseMS{}
 	_, err := makeAPIRequest("POST", repo.Config.Microservices.Finance.CurrentBudget, currentBudget, &res)
 	if err != nil {

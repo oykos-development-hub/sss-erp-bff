cc524e36595d77d413b6f420b0b71a6373023498
oykos-development-hub <hub@oykos.me>
2024-07-29 12:11:52 +0000

    Update code from GitLab

--- internal/api/graphql/resolvers/public_procurements.contract_articles.resolver.go
+++ internal/api/graphql/resolvers/public_procurements.contract_articles.resolver.go
@@ -81,7 +81,7 @@ func (r *Resolver) PublicProcurementContractArticlesOrganizationUnitResponseItem
 			grossValue = article.NetValue + article.NetValue*float32(vatPercentageFloat)/100
 		}
 
-		if amount > 0 {
+		if amount > 0 && articleData.VisibilityType == 1 {
 			items = append(items, dto.ProcurementContractArticlesResponseItem{
 				ID: article.PublicProcurementArticleID,
 				Article: dto.DropdownProcurementArticle{

f2cf863ab12213436f76460ab358bce3366bcdde
oykos-development-hub <hub@oykos.me>
2024-11-21 10:03:44 +0000

    Update code from GitLab

--- internal/api/sse/sse.go
+++ internal/api/sse/sse.go
@@ -64,6 +64,7 @@ func (sse *ServerSentEvent) Handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/event-stream")
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
+	w.Header().Set("X-Accel-Buffering", "no")
 
 	clientChan := sse.AddClient(user.ID)
 	defer func() {
